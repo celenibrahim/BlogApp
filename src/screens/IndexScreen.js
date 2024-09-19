60cbdf8837cefa64de22828b69df65a09b4df06b
celenibrahim <celen.ibrahim@outlook.com>
2024-09-19 22:01:04 +0300

    added delete option

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,34 @@
-import { FlatList, StyleSheet, Text, View, Button } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import React, { useContext } from "react";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 //import { Context as BlogContext } from "../context/BlogContext";
 const IndexScreen = () => {
-  const { state, addBlogPosts } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
-      <Button title="Add Blog" onPress={addBlogPosts} />
+      <Button title="Add Blog" onPress={addBlogPost} />
       <FlatList
         data={state}
-        keyExtractor={(blogPost) => blogPost.title}
+        keyExtractor={(blogPost) => blogPost.id}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
@@ -20,4 +37,19 @@ const IndexScreen = () => {
 
 export default IndexScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
